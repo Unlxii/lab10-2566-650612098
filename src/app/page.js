ef8b14b4e56c02c8f74c0a5e1f1cd9ee88848eda
tteenntt <87744652+Unlxii@users.noreply.github.com>
2023-07-24 16:14:43 +0700

    completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 "use client";
 
+import { UserCard } from "@/components/UserCard";
+import { cleanUser } from "@/libs/cleanUser";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function RandomUserPage() {
   //user = null or array of object
   const [users, setUsers] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [genAmount, setGenAmount] = useState(1);
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
 
   const generateBtnOnClick = async () => {
     setIsLoading(true);
@@ -16,10 +19,30 @@ export default function RandomUserPage() {
     );
     setIsLoading(false);
     const users = resp.data.results;
-    //Your code here
+    const morecleanUser = users.map((user) => cleanUser(user));
+    setUsers(morecleanUser);
+
+    //Your code herenp
     //Process result from api response with map function. Tips use function from /src/libs/cleanUser
-    //Then update state with function : setUsers(...)
+    //Then update state with functiosn : setUsers(...)
   };
+  useEffect(() => {
+    if (isFirstLoad) {
+      setIsFirstLoad(false);
+      return;
+    }
+    const strTasks = JSON.parse(genAmount);
+    localStorage.setItem("genAmount", strTasks);
+  }, [genAmount]);
+  useEffect(() => {
+    const strTasks = localStorage.getItem("genAmount");
+    if (strTasks === null) {
+      setGenAmount(1);
+      return;
+    }
+    //const loadedTasks = JSON.parse(strTasks);
+    setGenAmount(strTasks);
+  }, []);
 
   return (
     <div style={{ maxWidth: "700px" }} className="mx-auto">
@@ -40,7 +63,16 @@ export default function RandomUserPage() {
       {isLoading && (
         <p className="display-6 text-center fst-italic my-4">Loading ...</p>
       )}
-      {users && !isLoading && users.map(/*code map rendering UserCard here */)}
+      {users &&
+        !isLoading &&
+        users.map((user) => (
+          <UserCard
+            name={user.name}
+            imgUrl={user.imgUrl}
+            address={user.address}
+            email={user.email}
+          />
+        ))}
     </div>
   );
 }
